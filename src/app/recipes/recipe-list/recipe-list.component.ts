3e8ca52e8d73bf302995bbdf8b7407ac409f744a
pranav k <pranavkkeloth@gmail.com>
2023-10-19 14:34:35 +0530

    feat: added shopping list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -18,16 +18,6 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_800,h_400/https%3A%2F%2Fs3.amazonaws.com%2Fpixtruder%2Foriginal_images%2Ff5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
     ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_800,h_400/https%3A%2F%2Fs3.amazonaws.com%2Fpixtruder%2Foriginal_images%2Ff5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
-    ),
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto:eco,c_fill,g_auto,w_800,h_400/https%3A%2F%2Fs3.amazonaws.com%2Fpixtruder%2Foriginal_images%2Ff5cffedb779ce8ea3991f8020b5616d39ef6c0ee'
-    ),
   ];
 
   constructor() {}
