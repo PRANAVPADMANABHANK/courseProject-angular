0cd892b5911b89f1675f308fe9dc22b91fc25d50
pranav k <pranavkkeloth@gmail.com>
2023-10-20 12:37:47 +0530

    feat: Implement basic course project

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,16 @@
+// export class Ingredient{
+//     public name: String;
+//     public amount: number;
+
+//     constructor(name: String, amount: number){
+//         this.name = name;
+//         this.amount = amount;
+//     }
+// }
+
+                                     //---------------------- OR --------------------//
+
+
+export class Ingredient{
+    constructor(public name: String, public amount: number){}
+}
