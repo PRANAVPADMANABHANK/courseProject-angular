12d91492c57b477b5d9d7b358b051c61232483f6
pranav k <pranavkkeloth@gmail.com>
2023-12-08 11:30:45 +0530

    feat:added bug fix

--- src/app/payments/payments-routing.module.ts
+++ src/app/payments/payments-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PaymentsComponent } from './payments.component';
-
+             
 const routes: Routes = [{ path: '', component: PaymentsComponent }];
-
-@NgModule({
+                     
+@NgModule({              
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule]                         
 })
 export class PaymentsRoutingModule { }
-                             
+                                                    
+
+
+        
